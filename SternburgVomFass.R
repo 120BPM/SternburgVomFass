e051c3c6583fb2795d74f0c32c96b064d8228ca3
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2018-04-11 17:08:16 +0200

    Update SternburgVomFass.R

--- SternburgVomFass.R
+++ SternburgVomFass.R
@@ -1,5 +1,5 @@
 setwd("//clapton.wiwi.hu-berlin.de/irtg/IRTG/R2/SternburgVomFass")
 
-Sternburg = read.csv2("~/Desktop/HeartWatch/HeartWatchDetails.csv")
+Sternburg = read.csv2("~/Desktop/HeartWatch/HeuteEinKoenigWatchDetails.csv")
 date = substr(Sternburg$Datum, 1,10)
 boxplot(Sternburg$bpm~date, ylab="BPM")
